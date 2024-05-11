3e25a275a285e5f5aa3d6b1bdba3009fd6f31a4d
Travis Trego <travtrego@gmail.com>
2024-05-11 19:38:11 -0400

    Initial Commit

---
+++ hello.py
@@ -0,0 +1,24 @@
+import os
+os.system('clear')
+
+first_name = "Travis"
+walrus = "Boss"
+print (walrus)
+
+print (first_name) 
+walrus = "alpha"
+print (walrus)
+
+last_name = 'Trego' #all these are strings
+age = 41 # this is a number cuz no quotes
+names = ["Trav", "Katy", "Ed"] #0,1,2list
+print (names [2]) #2 would be ED
+
+#dictionary list of two things, seperate lines curly bracket, 
+#use colon and quotations to connect strings, print with regular brackest
+fav_food = {
+	"John": "Chicken", 
+	"Tim": "Pasta",
+	"Alfred" : "Pickles"
+}
+print (fav_food)
